fb39143e2834e165d7a7eb6ddda85952f2be1f60
katerin <ckaterinarellano@gmail.com>
2022-07-25 00:54:25 -0500

    Add service and constans

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import { showUsers } from "./service";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    showUsers().then((mainData) => {
+      const data = mainData.results.sort((a, b) => {
+        return a.registered.age - b.registered.age;
+      })
+      setUsers(data)
+    });
+  }, [])
+
+
   return (
-    <div>
-      Reto Tecnico
-    </div>
+    <table>
+
+    </table>
   );
 }
 
